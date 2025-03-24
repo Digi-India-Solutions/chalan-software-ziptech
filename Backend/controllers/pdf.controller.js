76d22ebb5757a483674da506f16c9925dd058422
hlwsatyam <satyampandit021@gmail.com>
2025-03-24 18:25:48 +0530

    upload

---
+++ Backend/controllers/pdf.controller.js
@@ -0,0 +1,27 @@
+import puppeteer from "puppeteer";
+
+const GeneratePdf = async (req, res) => {
+  try {
+    const { htmlContent } = req.body;
+
+    if (!htmlContent) {
+      return res.status(400).json({ message: "htmlContent is required" });
+    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+    
+    const pdfBuffer = await page.pdf({ format: "A4" });
+
+    await browser.close();
+    let filename = `invoice_${Date.now()}.pdf`;
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", `inline; filename=${filename}`);
+    return res.end(pdfBuffer);
+  } catch (error) {
+    console.log("PDF generation error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export { GeneratePdf };
