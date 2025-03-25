8ce00f8be8f5cf341aa969fcbf9f3fd897bf1fc1
nikunj kumar <nikunjkumar2911@gmail.com>
2025-03-25 15:38:51 +0530

    pdf fix

--- Backend/controllers/pdf.controller.js
+++ Backend/controllers/pdf.controller.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import pdf from "html-pdf-node";
 
 const GeneratePdf = async (req, res) => {
   try {
@@ -7,33 +7,20 @@ const GeneratePdf = async (req, res) => {
     if (!htmlContent) {
       return res.status(400).json({ message: "htmlContent is required" });
     }
-    const browser = await puppeteer.launch({
-      headless: "new", // Ensures headless mode
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--disable-software-rasterizer",
-        "--disable-extensions",
-        "--disable-features=site-per-process",
-        "--disable-blink-features=AutomationControlled",
-      ],
-    });
-    const page = await browser.newPage();
 
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" });
-    
-    const pdfBuffer = await page.pdf({ format: "A4" });
+    const options = { format: "A4" };
+    const file = { content: htmlContent };
 
-    await browser.close();
+    const pdfBuffer = await pdf.generatePdf(file, options);
+
     let filename = `invoice_${Date.now()}.pdf`;
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", `inline; filename=${filename}`);
     return res.end(pdfBuffer);
   } catch (error) {
     console.log("PDF generation error:", error);
-    res.status(500).json({ message: "Internal Server Error" ,error:error});
+    res.status(500).json({ message: "Internal Server Error", error });
   }
 };
+
 export { GeneratePdf };
